3769ba539557c094e4d4048ade082b72916acc0d
Jesica Gutierrez <jesica89@gmail.com>
2024-02-19 20:21:13 -0300

    js a mitad de camino

---
+++ proyecto-01-Swipe-Match-Tinder/functions.js
@@ -0,0 +1,60 @@
+const DECISION_THRESHOLD = 75
+
+let isAnimating = false
+let pullDeltaX = 0 // distancia que la card se está arrastrando
+
+function startDrag (event) {
+    if (isAnimating) return
+
+    // get the first article element
+    const actualCard = event.target.closest('article')
+
+    // get initial position of mouse or finger
+    const startX = event.pageX ?? event.touches[0].pageX
+
+    // listen the mouse and touch movemenets
+    document.addEventListener('mousemove', onMove)
+    document.addEventListener('mouseup', onEnd)
+
+    document.addEventListener('touchmove', onMove, {passive: true})
+    document.addEventListener('touchend', onEnd, {passive: true})
+
+    function onMove(event) {
+        // current position of mouse or finger
+        const currentX = event.pageX ?? event.touches[0].pageX
+        // the distance between the initial and current position
+        pullDeltaX = currentX - startX
+        // no hay distancia recorrida
+        if (pullDeltaX === 0) return
+        // change the flag to indicate we are animating
+        isAnimating = true
+        // calculate the rotation of the card using the distance
+        const deg = pullDeltaX / 10
+        // apply the transformation to the card
+        actualCard.style.transform = `translateX(${pullDeltaX}px)
+        rotate(${deg}deg)`
+        // change the cursor to grabbing
+        actualCard.style.cursor = 'grabbing'
+    }
+
+    function onEnd(event) {
+        // remove
+        document.addEventListener('mousemove', onMove)
+        document.addEventListener('mouseup', onEnd)
+
+        document.addEventListener('touchmove', onMove)
+        document.addEventListener('touchend', onEnd)
+
+        // saber si el usuario tomo una decision
+        const decisionMade = Math.abs(pullDeltaX) >= DECISION_THRESHOLD
+
+        if (decisionMade) {
+            console.log('decision hecha')
+        } else {
+            console.log('pensando...')
+        }
+    }
+}
+
+document.addEventListener('mousedown', startDrag)
+document.addEventListener('touchstart', startDrag, {passive: true})
